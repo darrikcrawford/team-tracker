62c0832b887d59c34abadeeed9f86374ae9f2462
Darrik Crawford <darrikcrawford@yahoo.com>
2021-11-17 23:31:11 -0600

    finish employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,7 +2,33 @@ const { test } = require('@jest/globals');
 const Employee = require('../lib/Employee.js');
 
 test('creates an employee object', () => {
-    const employee = new Employee('Darrik');
+    const employee = new Employee('Darrik',1,'darrik@email.com');
 
     expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String)); 
+});
+
+test("gets employee's name", () => {
+    const employee = new Employee('Darrik',1,'darrik@email.com');
+
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name)); 
+});
+
+test("gets employee's id value", () => {
+    const employee = new Employee('Darrik',1,'darrik@email.com');
+
+    expect(employee.getId()).toEqual(expect.stringContaining(employee.id.toString()));
+});
+
+test("gets employee's email address", () => {
+    const employee = new Employee('Darrik',1,'darrik@email.com');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email)); 
+});
+
+test("gets employee's role", () => {
+    const employee = new Employee('Darrik',1,'darrik@email.com');
+
+    expect(employee.getRole()).toEqual('Employee'); 
 });
